33226504762603c7b404e75a3211061c88d6072a
BlessmanJay <blessmanjay@gmail.com>
2025-07-25 07:33:52 +0100

    Update repository with new folders and files

--- interfaces/index.ts/index.ts
+++ interfaces/index.ts/index.ts
@@ -1,11 +1,17 @@
-export interface ButtonProps {
-  label: string;
-  onClick?: () => void;
-  type?: "button" | "submit" | "reset";
-}
-
-export interface CardProps {
-  title: string;
-  description: string;
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
   image: string;
 }
